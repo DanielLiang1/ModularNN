a589365a36d22e1dbb96373e62a740ba1654d8b7
Rafay Khan <rafayak16@gmail.com>
2018-10-27 18:04:06 +0500

    representational change in 'he' initialization

--- paramInitializer.py
+++ paramInitializer.py
@@ -20,7 +20,7 @@ def initialize_parameters(n_in, n_out, ini_type='plain'):
         # Delving Deep into Rectifiers: Surpassing Human-Level Performance on ImageNet Classification
         # Kaiming He et al. (https://arxiv.org/abs/1502.01852)
         # http: // cs231n.github.io / neural - networks - 2 /  # init
-        params['W'] = np.random.randn(n_out, n_in) / np.sqrt(n_in/2)  # set variance of W to 2/n
+        params['W'] = np.random.randn(n_out, n_in) * np.sqrt(2/n_in)  # set variance of W to 2/n
 
     params['b'] = np.zeros((n_out, 1))    # set bias 'b' to zeros
 
